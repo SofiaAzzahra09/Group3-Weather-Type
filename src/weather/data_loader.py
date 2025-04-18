e4b9439042dd5a5455272f988009fff975b0d76e
Sofia Azzahra <sofiaazzahra0001@gmail.com>
2025-04-18 15:29:23 +0700

    create project & initial commit

---
+++ src/weather/data_loader.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def load_data(file_path: str) -> pd.DataFrame:
+    try:
+        data = pd.read_csv(file_path)
+        return data
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        return pd.DataFrame()
